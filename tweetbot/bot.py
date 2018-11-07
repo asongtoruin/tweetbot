f2f206980241973aab5ac7bddafdda5df8e1228d
Adam <adam.ruszkowski@gmail.com>
2018-11-07 22:33:58 +0000

    Basic API setup :baby_chick:

--- tweetbot/bot.py
+++ tweetbot/bot.py
@@ -2,8 +2,34 @@ import tweepy
 
 
 class TweetBot:
-    def __init__(self):
-        # TODO set up API, read keys etc
-        pass
+    def __init__(self, key_source='values',
+                 consumer_key=None, consumer_secret=None,
+                 access_token=None, access_token_secret=None):
+        if key_source not in ('values', 'files'):
+            raise ValueError('key_value should be one of values, files or env')
 
+        consumer_keys = (consumer_key, consumer_secret)
+        access_keys = (access_token, access_token_secret)
 
+        if key_source == 'files':
+            consumer_keys = map(self.read_key_from_file, consumer_keys)
+            access_keys = map(self.read_key_from_file, access_keys)
+
+        auth = tweepy.OAuthHandler(*consumer_keys)
+        auth.set_access_token(*access_keys)
+
+        self.api = tweepy.API(auth)
+
+        user = self.api.me()
+        print(user)
+        self.screen_name = user.screen_name
+        print(self.screen_name)
+
+    @staticmethod
+    def read_key_from_file(input_file):
+        with open(input_file, 'r') as f:
+            return f.read()
+
+    def post_photo(self, tweet_text, **kwargs):
+        from .camera import take_photo
+        self.api.update_with_media(take_photo(), status=tweet_text, **kwargs)
